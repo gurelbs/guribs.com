cbb0f8592edf7aedaff3b5584382670cc5039b6a
gurelbs <guri240@gmail.com>
2021-09-17 03:35:03 +0300

    updated

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors')
-app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
